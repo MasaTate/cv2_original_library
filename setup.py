2ce17d3c69802ace001020931caba66c5f67ecc5
MasaTate <tatemasaaac@gmail.com>
2023-01-18 15:16:12 +0900

    changed library name

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ from setuptools import setup, find_packages
 install_requires = ["cv2", "PIL", "numpy"]
 
 packages = ["transform", "video"]
-setup(name="cv2utils", version="1.0", packages=find_packages())
+setup(
+    name="cv2utils", version="1.0", packages=packages, install_requires=install_requires
+)
