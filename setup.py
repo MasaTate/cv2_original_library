19b286e8605b6484a6dcf32151b78f676dfb479d
MasaTate <tatemasaaac@gmail.com>
2023-01-18 15:45:10 +0900

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires = ["opencv-python", "Pillow", "numpy"]
 packages = ["cv2utils"]
 setup(
     name="cv2utils",
-    version="1.1",
+    version="1.2",
     packages=packages,
     install_requires=install_requires,
 )
